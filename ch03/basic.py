60bc6921b174505aa5fea708b1438bb78b46d43b
OctopusLian <zoctopus@qq.com>
2022-08-30 14:55:25 +0800

    add 第三章

---
+++ ch03/basic.py
@@ -0,0 +1,9 @@
+'''
+Author: neozhang
+Date: 2022-08-30 14:49:12
+LastEditors: neozhang
+LastEditTime: 2022-08-30 14:52:54
+Description: 请填写简介
+'''
+print(1+2)
+print(1 + 2 * 5)
---
+++ ch03/string.py
@@ -0,0 +1,16 @@
+'''
+Author: neozhang
+Date: 2022-08-30 14:51:55
+LastEditors: neozhang
+LastEditTime: 2022-08-30 14:51:56
+Description: 请填写简介
+'''
+"""
+打印字符串的程序
+加上注释的例子
+"""
+# print('你好，Python！')
+print('Hello， “GREAT” Python!!')  # Hello， “GREAT” Python!!
+print('今年', 16,
+      '岁。还有', 18 - 16,
+      '年就成年了。好想快点长大。') # 今年 16 岁。还有 2 年就成年了。好想快点长大。
